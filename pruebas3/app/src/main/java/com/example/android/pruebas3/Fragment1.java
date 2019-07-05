ba56ce7b5aaab6de44cdf45709820ba63af72512
LeoBrasileo <leobraginski@gmail.com>
2019-07-05 11:51:35 -0300

    ESTA FUNCANDO

--- pruebas3/app/src/main/java/com/example/android/pruebas3/Fragment1.java
+++ pruebas3/app/src/main/java/com/example/android/pruebas3/Fragment1.java
@@ -21,25 +21,14 @@ import com.google.zxing.Result;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Fragment1.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Fragment1#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Fragment1 extends Fragment implements ZXingScannerView.ResultHandler {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+public class Fragment1 extends Fragment implements ZXingScannerView.ResultHandler
+{
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
     private FirebaseDatabase database;
     private DatabaseReference qrs;
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -47,21 +36,21 @@ public class Fragment1 extends Fragment implements ZXingScannerView.ResultHandle
     private ZXingScannerView zXingScannerView;
 
     @Override
-    public void onResume() {
+    public void onResume()
+    {
         super.onResume();
         zXingScannerView.setResultHandler(this);
         zXingScannerView.startCamera();
     }
 
     @Override
-    public void onPause() {
+    public void onPause()
+    {
         super.onPause();
         zXingScannerView.stopCamera();
     }
 
-    public Fragment1() {
-        // Required empty public constructor
-    }
+    public Fragment1() {}
 
     public static Fragment1 newInstance(String param1, String param2)
     {
@@ -74,7 +63,8 @@ public class Fragment1 extends Fragment implements ZXingScannerView.ResultHandle
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         if (getArguments() != null)
         {
@@ -84,31 +74,37 @@ public class Fragment1 extends Fragment implements ZXingScannerView.ResultHandle
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)
+    {
         zXingScannerView = new ZXingScannerView(getActivity());
         return zXingScannerView;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
+    public void onButtonPressed(Uri uri)
+    {
+        if (mListener != null)
+        {
             mListener.onFragmentInteraction(uri);
         }
     }
 
     @Override
-    public void onAttach(Context context) {
+    public void onAttach(Context context)
+    {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
+        if (context instanceof OnFragmentInteractionListener)
+        {
             mListener = (OnFragmentInteractionListener) context;
-        } else {
+        } else
+            {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
         }
     }
 
     @Override
-    public void onDetach() {
+    public void onDetach()
+    {
         super.onDetach();
         mListener = null;
     }
@@ -150,26 +146,18 @@ public class Fragment1 extends Fragment implements ZXingScannerView.ResultHandle
 
 
         Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
+        handler.postDelayed(new Runnable()
+        {
             @Override
-            public void run() {
+            public void run()
+            {
+
                 zXingScannerView.resumeCameraPreview(Fragment1.this);
             }
         }, 2000);
     }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
+    public interface OnFragmentInteractionListener
+    {
         void onFragmentInteraction(Uri uri);
     }
 }
