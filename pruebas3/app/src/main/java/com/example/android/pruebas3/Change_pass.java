aa0fc51b98cfea45938ce213480905ee3a93c454
LeoBrasileo <leobraginski@gmail.com>
2019-07-04 21:15:39 -0300

    Hacer activity pass change

--- pruebas3/app/src/main/java/com/example/android/pruebas3/Change_pass.java
+++ pruebas3/app/src/main/java/com/example/android/pruebas3/Change_pass.java
@@ -3,11 +3,17 @@ package com.example.android.pruebas3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class Change_pass extends AppCompatActivity
 {
     private Toolbar toolbar;
+    private Button validar;
+    private EditText oldpass;
+    private EditText newpass1;
+    private EditText newpass2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +21,11 @@ public class Change_pass extends AppCompatActivity
         setContentView(R.layout.activity_change_pass);
 
         toolbar = findViewById(R.id.toolbar2);
+        oldpass = findViewById(R.id.editPass4);
+        newpass1 = findViewById(R.id.editPass5);
+        newpass2 = findViewById(R.id.editPass6);
+        validar = findViewById(R.id.validar4);
+
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Cambiar contraseña");
