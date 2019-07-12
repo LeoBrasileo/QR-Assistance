76f7915da690b44ac68adec4407a19c5c8d2c9e6
Dan Farach <danfarach18@gmail.com>
2019-07-12 10:08:09 -0300

    danfa notificaciones sin probar

---
+++ pruebas3/app/src/main/java/com/example/android/pruebas3/ConfNotificaciones.java
@@ -0,0 +1,37 @@
+package com.example.android.pruebas3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Switch;
+import android.widget.Toast;
+
+public class ConfNotificaciones extends AppCompatActivity {
+private Switch Sw;
+
+String str1, str2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Switch sb = new Switch(this);
+        Sw = (Switch)findViewById(R.id. switch1);
+        sb.setTextOff("OFF");
+        sb.setTextOn("ON");
+        sb.setChecked(true);
+
+
+
+        if (Sw. isChecked()){
+            str1 = Sw.getTextOn ().toString();
+            Toast.makeText(getApplicationContext(), "Notificaciones prendidas",Toast.LENGTH_SHORT).show();
+        }
+        else {
+            str1 = Sw.getTextOff().toString();
+            Toast.makeText(getApplicationContext(), "Notificaciones apagadas",Toast.LENGTH_SHORT).show();
+        }
+
+
+
+    }
+}
