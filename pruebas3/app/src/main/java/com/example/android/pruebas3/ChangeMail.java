95163d7edd777abc9bde519f7f8583eee4c247b4
Leonel Braginski <leobraginski@gmail.com>
2019-07-12 09:05:55 -0300

    chage email en trabajo

--- pruebas3/app/src/main/java/com/example/android/pruebas3/ChangeMail.java
+++ pruebas3/app/src/main/java/com/example/android/pruebas3/ChangeMail.java
@@ -2,14 +2,15 @@ package com.example.android.pruebas3;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -18,34 +19,22 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-
-public class AddMail extends AppCompatActivity {
+public class ChangeMail extends AppCompatActivity
+{
     private Toolbar toolbar;
-    private EditText mail;
-    private Button validar;
     private FirebaseDatabase database;
     private DatabaseReference users;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate (Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_mail);
+        setContentView(R.layout.activity_change_mail);
         toolbar = findViewById(R.id.toolbar2);
-        mail = findViewById(R.id.addMail1);
-        validar = findViewById(R.id.btnAddEmail);
-        database = FirebaseDatabase.getInstance();
-        users = database.getReference("usuarios");
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setTitle("Registrar un mail");
-
-        validar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                RegistrarMail(mail.getText().toString());
-            }
-        });
+        getSupportActionBar().setTitle("Cambiar Email");
     }
 
     private void RegistrarMail (final String mail)
@@ -56,7 +45,7 @@ public class AddMail extends AppCompatActivity {
             return;
         }
         final ProgressDialog progressDialog = new ProgressDialog(this);
-        progressDialog.setMessage("Cambiando Email");
+        progressDialog.setMessage("Registrando Email");
         progressDialog.show();
         Bundle bundle = getIntent().getExtras();
         final String dni = bundle.getString("dni");
@@ -66,15 +55,15 @@ public class AddMail extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot)
             {
-                ObjetoUsuario chmail = dataSnapshot.child(dni).getValue(ObjetoUsuario.class);
-                chmail.setEmail(mail);
-                users.child(dni).setValue(chmail);
+                ObjetoUsuario addmail = dataSnapshot.child(dni).getValue(ObjetoUsuario.class);
+                addmail.setEmail(mail);
+                users.child(dni).setValue(addmail);
                 Intent intent =
-                        new Intent(AddMail.this,NavDrawer.class);
+                        new Intent(ChangeMail.this,NavDrawer.class);
                 Bundle bundle = new Bundle();
-                bundle.putString("user", chmail.getNombre().toString());
-                bundle.putString("mail", chmail.getEmail().toString());
-                bundle.putString("dni", chmail.getDni().toString());
+                bundle.putString("user", addmail.getNombre().toString());
+                bundle.putString("mail", addmail.getEmail().toString());
+                bundle.putString("dni", addmail.getDni().toString());
                 intent.putExtras(bundle);
                 progressDialog.dismiss();
                 startActivity(intent);
