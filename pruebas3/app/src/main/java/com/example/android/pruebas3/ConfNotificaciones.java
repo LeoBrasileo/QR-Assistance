702bc06dc427389ad14a1da0ed221608492d105d
Leonel Braginski <leobraginski@gmail.com>
2019-07-12 12:00:23 -0300

    Notificaciones de danfa

--- pruebas3/app/src/main/java/com/example/android/pruebas3/ConfNotificaciones.java
+++ pruebas3/app/src/main/java/com/example/android/pruebas3/ConfNotificaciones.java
@@ -1,5 +1,7 @@
 package com.example.android.pruebas3;
+import android.app.Notification;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.CompoundButton;
 import android.widget.Switch;
@@ -9,14 +11,13 @@ import android.widget.Toast;
 public class ConfNotificaciones extends AppCompatActivity {
 
     Switch switchButton;
-
+    public boolean Notificaciones = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.switch_button_example);
 
-        // For first switch button
         switchButton = (Switch) findViewById(R.id.switchButton);
 
 
@@ -26,11 +27,13 @@ public class ConfNotificaciones extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton compoundButton, boolean bChecked) {
                 if (switchButton.isChecked()) {
                     Toast.makeText(getApplicationContext(), "Notificaciones prendidas",Toast.LENGTH_SHORT).show();
+                    Notificaciones = true;
                     switchButton.setChecked(true);
                 }
 
                 else {
                     Toast.makeText(getApplicationContext(), "Notificaciones apagadas",Toast.LENGTH_SHORT).show();
+                    Notificaciones = false;
                     switchButton.setChecked(false);
                 }
 
