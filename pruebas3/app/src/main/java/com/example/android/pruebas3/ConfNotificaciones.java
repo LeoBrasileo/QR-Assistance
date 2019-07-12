17d868e85e7fba538c1dc12ee046871f049feea9
Dan Farach <danfarach18@gmail.com>
2019-07-12 11:13:44 -0300

    NOTIFICACIONES TERMINADO (danfa)

--- pruebas3/app/src/main/java/com/example/android/pruebas3/ConfNotificaciones.java
+++ pruebas3/app/src/main/java/com/example/android/pruebas3/ConfNotificaciones.java
@@ -1,37 +1,44 @@
 package com.example.android.pruebas3;
-
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
 
-public class ConfNotificaciones extends AppCompatActivity {
-private Switch Sw;
 
-String str1;
+public class ConfNotificaciones extends AppCompatActivity {
+
+    Switch switchButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.switch_button_example);
 
-        Switch sb = new Switch(this);
-        Sw = findViewById(R.id. switch1);
-        sb.setTextOff("OFF");
-        sb.setTextOn("ON");
-        sb.setChecked(true);
+        // For first switch button
+        switchButton = (Switch) findViewById(R.id.switchButton);
 
 
+        switchButton.setChecked(true);
+        switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean bChecked) {
+                if (switchButton.isChecked()) {
+                    Toast.makeText(getApplicationContext(), "Notificaciones prendidas",Toast.LENGTH_SHORT).show();
+                    switchButton.setChecked(true);
+                }
 
-        if (Sw. isChecked()){
-            str1 = Sw.getTextOn ().toString();
-            Toast.makeText(getApplicationContext(), "Notificaciones prendidas",Toast.LENGTH_SHORT).show();
-        }
-        else {
-            str1 = Sw.getTextOff().toString();
-            Toast.makeText(getApplicationContext(), "Notificaciones apagadas",Toast.LENGTH_SHORT).show();
-        }
+                else {
+                    Toast.makeText(getApplicationContext(), "Notificaciones apagadas",Toast.LENGTH_SHORT).show();
+                    switchButton.setChecked(false);
+                }
+
+            }
+        });
 
 
 
     }
+
 }
