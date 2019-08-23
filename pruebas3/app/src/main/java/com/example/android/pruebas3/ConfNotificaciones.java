d303e533ae1e90f7db05af87f617a78054bada7f
danfa18 <danfarach18@gmail.com>
2019-08-23 13:27:47 -0300

    YA FUNCIONA, No jodo

--- pruebas3/app/src/main/java/com/example/android/pruebas3/ConfNotificaciones.java
+++ pruebas3/app/src/main/java/com/example/android/pruebas3/ConfNotificaciones.java
@@ -13,7 +13,6 @@ public class ConfNotificaciones extends AppCompatActivity {
     public boolean Notificaciones = false;
     private Toolbar toolbar;
 
-    //Lolazo
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
