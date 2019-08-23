47cb5c8c8ffcad96318f4d0aab448b5113feea27
danfa18 <danfarach18@gmail.com>
2019-08-23 09:10:38 -0300

    Ultima prueba Danfa

--- pruebas3/app/src/main/java/com/example/android/pruebas3/ConfNotificaciones.java
+++ pruebas3/app/src/main/java/com/example/android/pruebas3/ConfNotificaciones.java
@@ -19,6 +19,8 @@ public class ConfNotificaciones extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notificaciones);
 
+
+
         switchButton = (Switch) findViewById(R.id.switchButton);
 
         toolbar = findViewById(R.id.toolbar2);
