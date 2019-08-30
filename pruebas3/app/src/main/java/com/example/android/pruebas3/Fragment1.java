a2c606001342598584d204f8af9339de06b3e37e
LeoBrasileo <leobraginski@gmail.com>
2019-08-29 22:23:37 -0300

    Viene bien la cosa

--- pruebas3/app/src/main/java/com/example/android/pruebas3/Fragment1.java
+++ pruebas3/app/src/main/java/com/example/android/pruebas3/Fragment1.java
@@ -258,13 +258,11 @@ public class Fragment1 extends Fragment implements ZXingScannerView.ResultHandle
                                     DatabaseReference inasistenciaramamateria = inasistencias.child(idActual);
                                     DatabaseReference inasistenciaramadia = inasistenciaramamateria.child(fechachild);
 
-                                    //inasistenciaramadia.child("presentes").setValue(bundle.getString("user"));
                                     inasistenciaramadia.child("presentes").child(bundle.getString("dni")).setValue(bundle.getString("user"));
 
                                     Toast.makeText(getContext(),"Estas presente",Toast.LENGTH_LONG).show();
 
                                     //tengo que conseguir el dato de cuanto alumnos ya se marcaron presentes y segun eso agregar al proximo
-
                                     //si ya se presentaron 5 alumnos entonces el proximo que se presente se va a guardar
                                     //en el valor de la rama String Presente6.
                                 }
@@ -291,12 +289,6 @@ public class Fragment1 extends Fragment implements ZXingScannerView.ResultHandle
                 Toast.makeText(getContext(),"Error",Toast.LENGTH_LONG).show();
             }
         });
-
-
-        //Toda la parte tecnica de la app se va a programar aca
-
-
-
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable()
