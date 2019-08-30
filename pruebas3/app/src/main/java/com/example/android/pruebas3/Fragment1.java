6ac9dbd7be1c423de52ec1b20daef724839c5b6d
LeoBrasileo <leobraginski@gmail.com>
2019-08-29 22:20:48 -0300

    Viene bien la cosa

--- pruebas3/app/src/main/java/com/example/android/pruebas3/Fragment1.java
+++ pruebas3/app/src/main/java/com/example/android/pruebas3/Fragment1.java
@@ -242,7 +242,6 @@ public class Fragment1 extends Fragment implements ZXingScannerView.ResultHandle
                         @Override
                         public void onDataChange(@NonNull DataSnapshot dataSnapshot)
                         {
-                            //getHorarioActualForInasistencias();
                             diaactual = division.child(day);
                             materiaact1 = diaactual.child(horariosmaterias);
 
@@ -259,20 +258,22 @@ public class Fragment1 extends Fragment implements ZXingScannerView.ResultHandle
                                     DatabaseReference inasistenciaramamateria = inasistencias.child(idActual);
                                     DatabaseReference inasistenciaramadia = inasistenciaramamateria.child(fechachild);
 
+                                    //inasistenciaramadia.child("presentes").setValue(bundle.getString("user"));
+                                    inasistenciaramadia.child("presentes").child(bundle.getString("dni")).setValue(bundle.getString("user"));
+
+                                    Toast.makeText(getContext(),"Estas presente",Toast.LENGTH_LONG).show();
+
                                     inasistenciaramadia.child("presentes").addListenerForSingleValueEvent(new ValueEventListener() {
                                         @Override
                                         public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                                            //tengo que conseguir el dato de cuanto alumnos ya se marcaron presentes
-                                            //y segun eso agregar al proximo
+                                            //tengo que conseguir el dato de cuanto alumnos ya se marcaron presentes y segun eso agregar al proximo
 
                                             //si ya se presentaron 5 alumnos entonces el proximo que se presente se va a guardar
                                             //en el valor de la rama String Presente6.
                                         }
 
                                         @Override
-                                        public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                                        }
+                                        public void onCancelled(@NonNull DatabaseError databaseError) { }
                                     });
                                 }
 
