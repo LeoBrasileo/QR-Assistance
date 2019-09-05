fbbf29d2ddeffd94b5eb562c1772bce4bbefbcab
Android <android@ort>
2019-09-05 09:56:42 -0300

    cambios menores

--- firecast/functions/index.js
+++ firecast/functions/index.js
@@ -4,20 +4,27 @@ const path = require("path");
 const cors = require("cors")({ origin: true });
 const fs = require("fs");
 
+var currentTime = new Date();
+var currentHours = currentTime.getHours();
+var currentMinutes = currentTime.getMinutes();
+var currentSeconds = currentTime.getSeconds();
+
+currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+currentHours = currentHours - 3;
+currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+currentHours = ( currentHours === 0 ) ? 12 : currentHours;
+var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
+var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+
 const admin = require('firebase-admin');
 admin.initializeApp();
 
  exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("🍕🍕🍕🍕🍕🍕🍕🍕");
+  response.send(currentTimeString);
  });
 
-exports.addMessageForTest1 = functions.https.onRequest(async (req, res) => {
-      const mensaje = "no quiero mas childs";
-      const snapshot = await admin.database().ref('javascript').push(mensaje);
-      res.redirect(303, snapshot.ref.toString());
-    });
-
-exports.addMessageForTest2 = functions.https.onRequest(async (req, res) => {
+exports.addMessageForDatabase1 = functions.https.onRequest(async (req, res) => {
       const mensaje = "ProBande3lOsbdYHBjbfe";
       const snapshot = await admin.database().ref('javascript').push(mensaje);
       res.redirect(303, snapshot.ref.toString());
